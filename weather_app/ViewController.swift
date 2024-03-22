f44a7a5204fef5545fc43e1cc1c4c289a8f34bc5
maxzim121 <zimensm12@gmail.com>
2024-03-22 14:05:03 +0300

    Deleted Main.Storyboard

--- weather_app/ViewController.swift
+++ weather_app/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  weather_app
-//
-//  Created by Maksim Zimens on 22.03.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
